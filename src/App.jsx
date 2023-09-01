a60a50d4fdfe9d2d02d3d713b34a9dbfb750c0d0
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-09-01 00:04:04 -0400

    primero ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,75 @@
+import { useState } from "react";
+
 import { Title } from "./components/Title";
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
 import { Todo } from "./components/Todo";
 
 function App() {
+
+  const [todos,setTodos] = useState([
+    {
+      id:1,
+      title: 'Watch the next Marvel Movie',
+      completed: false,
+    },
+    {
+      id:2,
+      title: 'Record the next Video',
+      completed: false,
+    },
+    {
+      id:3,
+      title: 'Wash the dishes',
+      completed: false,
+    },
+    {
+      id:4,
+      title: 'Study 2 hours',
+      completed: false,
+    }
+  ]);
+
+  const addTodo = (title) => {
+
+      const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+
+      const newTodo = {
+        id: lastId + 1,
+        title,
+        completed: false
+      }
+
+      const todoList = [...todos];
+      todoList.push(newTodo);
+      setTodos(todoList);
+  }
+
+  const handleSetComplete = (id) => {
+    const updateList = todos.map(todo => {
+      if(todo.id === id){
+        return { ...todo, completed: !todo.completed }
+      }
+      return todo;
+    });
+    setTodos(updateList);
+  }
+
+  const handleDelete = (id) => {
+    const updateList = todos.filter(todo => todo.id !== id);
+    setTodos(updateList);
+  }
+
   return (
     <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
-        <TodoInput />
-        <TodoList>
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-        </TodoList>
+        <TodoInput addTodo = {addTodo}/>
+        <TodoList 
+          todos={todos} 
+          handleSetComplete = {handleSetComplete}
+          handleDelete = {handleDelete}
+        />
       </div>
     </div>
   );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,20 @@
 import { TodoFilters } from "../TodoFilters/TodoFilters"
+import { Todo } from "../Todo/Todo";
 
 
-const TodoList = ({ children }) => {
+const TodoList = ({ todos , handleSetComplete , handleDelete }) => {
   return (
     <div className="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl">
-        { children }
+        {todos.map(todo => {
+          return (
+            <Todo 
+              key={todo.id} 
+              todo={todo} 
+              handleSetComplete= {handleSetComplete}
+              handleDelete = {handleDelete}  
+            />
+          )
+        })}
         <TodoFilters />
     </div>
   )
