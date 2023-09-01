47daa333d68d86be8d47bf766797e9dd8bde27a5
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-08-31 21:50:57 -0400

    iconos agregados

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Title } from "./components/Title";
+import { TodoInput } from "./components/TodoInput";
+import { TodoList } from "./components/TodoList";
+import { Todo } from "./components/Todo";
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+        <Title />
+        <TodoInput />
+        <TodoList>
+          <Todo />
+        </TodoList>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,7 +2,14 @@
 
 const Todo = () => {
   return (
-    <div className="flex items-center justify-between p-4 bg-gray-700 border-b border-solid border-gray-600">Todo</div>
+    <div className="flex items-center justify-between p-4 bg-gray-700 border-b border-solid border-gray-600">
+        <div className="flex items-center">
+            <span className="border-solid border border-gray-500 rounded-full p-3 cursor-pointer"></span>
+            <p className="pl-3">
+                Todo Item
+            </p>
+        </div>        
+    </div>
   )
 }
 
